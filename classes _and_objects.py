2d00892a9e04e47b59f725bfa8f4320eab966cd8
Ayushman Dwivedi <ayushmandwivedi732@gmail.com>
2026-02-24 15:43:19 +0530

    Add files via upload

---
+++ classes _and_objects.py
@@ -0,0 +1,21 @@
+#classes # template or blueprint of the object 
+
+#creating a class
+
+class Myclass:
+    pass
+
+
+#creating a object
+obj1 = Myclass()
+obj2 = Myclass()
+print(obj1)
+print(obj2)
+
+# obj1 and obj2 are objects of class Myclass
+
+l1 = (10,45,20)
+print(type(l1)) 
+
+print(type(obj1))
+print(type(obj2))
